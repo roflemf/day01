95b0b4be1da35e52f57209cc555143429cabe99c
test <test@test.com>
2023-05-17 19:49:42 +0900

    실습1

--- src/day01/TestClass03.java
+++ src/day01/TestClass03.java
@@ -1,5 +1,24 @@
 package day01;
 
 public class TestClass03 {
+	
+	public static void main(String[] args) {
+		System.out.println(100 + 100);
+		System.out.println(100 + 100.123);
+		
+		System.out.println("백"+100+200);
+		System.out.println("백"+(100+200));
+		
+		System.out.println("안녕"+"하세요");
+		
+		System.out.println("안녕"+500+300);
+		
+		
+		
+		
+		
+		
+		
+	}
 
 }
